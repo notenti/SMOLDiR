8032893bcbebbb1c5d997672911f55d5ab6c5a85
Nate Otenti <otenti.nate@gmail.com>
2017-09-08 00:37:51 -0400

    Added some more database stuff

--- app/src/main/java/com/v4/nate/smokedetect/DetectorHealthFragment.java
+++ app/src/main/java/com/v4/nate/smokedetect/DetectorHealthFragment.java
@@ -2,12 +2,16 @@ package com.v4.nate.smokedetect;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DetectorHealthFragment extends Fragment {
 
@@ -20,6 +24,22 @@ public class DetectorHealthFragment extends Fragment {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //This method is called once with the initial value and again
+                //whenever data at this location is updated
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("EHH", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //Failed to read value
+                Log.w("EHH", "Failed to read value.", databaseError.toException());
+            }
+        });
         return view;
     }
 }
